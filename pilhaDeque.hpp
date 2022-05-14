e35000c7f615ef424a31587b66a818bec5489a2c
raphael.goncalves <raphaelricardo10@gmail.com>
2022-05-14 09:58:58 -0300

    Criada a classe PilhaDeque

---
+++ pilhaDeque.hpp
@@ -0,0 +1,33 @@
+#include "deque.hpp"
+
+template <typename Agregado, typename Tipo>
+concept PilhaTAD = requires(Agregado a, Tipo t)
+{
+    // requer operação 'empilha' sobre tipo 't'
+    {a.empilha(t)};
+    // requer operação 'desempilha' e retorna tipo 't'
+    {a.desempilha()};
+    // requer operação 'topo' e retorna tipo 't'
+    {a.topo()};
+};
+
+class PilhaDeque
+{
+public:
+    Deque d;
+
+    void empilha(char c)
+    {
+        this->d.insereFim(c);
+    }
+    char desempilha()
+    {
+        return this->d.removeFim();
+    }
+    char topo()
+    {
+        return this->d.fim();
+    }
+};
+
+static_assert(PilhaTAD<PilhaDeque, char>);
